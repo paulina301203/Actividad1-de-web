623db96c042cb8879acbdc79d3d5e3c5237012f4
Pao-Molina <pmova13@gmail.com>
2026-08-01 10:35:37 -0500

    Primer commit

---
+++ src/main/java/com/example/Product.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Product {
+    private String id;
+    private String name;
+    private String price;
+    private String stock;
+
+    public String getId() {
+        return id;
+    }
+}
